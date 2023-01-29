fcf7af566dfd1342687915baf9d8f418ec780eea
Jacob Roth <jacob@redhawk-tech.com>
2023-01-29 03:18:27 -0500

    adding password character options

--- js/random.js
+++ js/random.js
@@ -1,16 +1,26 @@
+//#region CONST
 const caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lows = 'abcdefghijklmnopqrstuvwxyz';
-const nums = '1234567890';
+const nums = '0123456789';
 const spec = '!@#$%^&*()-_=+,./\\|`~;:\'"[]{}';
 
 const lengthId = 'intLength';
 const outputId = 'textOutput';
 const lengthValueId = 'intLengthValue';
+const optionsButton = 'btnShowMore';
+const optionsDiv = 'divOptions';
 const defaultLength = 28;
 
+const checkboxCapitals = 'checkCaps';
+const checkboxLowercase = 'checkLows';
+const checkboxNumbers = 'checkNums';
+const checkboxSpecials = 'checkSpec';
+
+const hideOptionsClass = 'hidden';
+//#endregion CONST
+
 function generatePassword() {
-  // TODO in the future, allow users to check what boxes to use
-  const chosenChars = caps + lows + nums + spec;
+  const chosenChars = getAvailableCharacters();
   const length = document.getElementById(lengthId).value;
 
   var password = '';
@@ -20,6 +30,15 @@ function generatePassword() {
   }
 
   setOutput(password);
+}
+
+function getAvailableCharacters() {
+  let chars = '';
+  chars += document.getElementById(checkboxCapitals).checked ? caps : '';
+  chars += document.getElementById(checkboxLowercase).checked ? lows : '';
+  chars += document.getElementById(checkboxNumbers).checked ? nums : '';
+  chars += document.getElementById(checkboxSpecials).checked ? spec : '';
+  return chars;
 }
 
 function updateLengthLabel() {
@@ -46,7 +65,19 @@ function copyToClipboard() {
   navigator.clipboard.writeText(pw);
 }
 
+function createOptionsEvents() {
+  document.getElementById(optionsButton).addEventListener('click', (ev) => {
+    document.getElementById(optionsDiv).classList.toggle(hideOptionsClass);
+    document.getElementById(optionsButton).innerHTML = document
+      .getElementById(optionsDiv)
+      .classList.contains(hideOptionsClass)
+      ? '&and;'
+      : '&or;';
+  });
+}
+
 window.onload = function () {
   updateLengthLabel();
   setOutput('');
+  createOptionsEvents();
 };
