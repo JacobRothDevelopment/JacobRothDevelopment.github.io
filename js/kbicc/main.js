37f432a1d7e80e7169c3e376a047c62c4da6430d
JacobRothDevelopment <rothy784715+github@gmail.com>
2023-02-18 00:00:15 -0500

    more

--- js/kbicc/main.js
+++ js/kbicc/main.js
@@ -1,10 +1,29 @@
 //#region CONST
-const keyId = 'textKey';
-const inputId = 'textInput';
-const outputId = 'textOutput';
-const btnNewKey = 'btnNewKey';
-const btnEncrypt = 'btnEncrypt';
-const btnReset = 'btnReset';
+const conf = {
+  ids: {
+    key: 'textKey',
+    input: 'textInput',
+    output: 'textOutput',
+    btnNewKey: 'btnNewKey',
+    btnEncrypt: 'btnEncrypt',
+    btnReset: 'btnReset',
+    isEncrypt: 'chkIsEncrypt',
+    buttonName: 'textButtonTitle',
+    titleName: 'textTitleTitle',
+  },
+  checkboxOptions: {
+    false: {
+      title: 'Encryption',
+      label: 'Encrypt',
+      function: runEncryption,
+    },
+    true: {
+      title: 'Decryption',
+      label: 'Decrypt',
+      function: runDecryption,
+    },
+  },
+};
 
 const defaultKeyLength = 10;
 
@@ -26,49 +45,75 @@ const allowedKeyInputKeys = [
 
 function setNewKey() {
   /** @type {HTMLInputElement} */
-  let key = document.getElementById(keyId);
+  let key = document.getElementById(conf.ids.key);
   key.value = genKey(defaultKeyLength);
 }
 
 function runEncryption() {
-  let message = document.getElementById(inputId).value;
-  let key = document.getElementById(keyId).value;
+  let message = document.getElementById(conf.ids.input).value;
+  let key = document.getElementById(conf.ids.key).value;
   let cryptHexList = Encrypt(message, key);
   let cryptString = charHexArrayToString(cryptHexList);
-  document.getElementById(outputId).value = cryptString;
+  document.getElementById(conf.ids.output).innerHTML = cryptString;
+}
+
+function runDecryption() {
+  let message = document.getElementById(conf.ids.input).value;
+  let messageHex = stringToCharHexArray(message);
+  let key = document.getElementById(conf.ids.key).value;
+  let cryptHexList = Decrypt(messageHex, key);
+  let cryptString = charHexArrayToString(cryptHexList);
+  document.getElementById(conf.ids.output).innerHTML = cryptString;
+}
+
+function runCryptFunction() {
+  let isDecrypt = document.getElementById(conf.ids.isEncrypt).checked;
+  conf.checkboxOptions[isDecrypt].function();
 }
 
 function createEventListeners() {
-  document.getElementById(inputId).addEventListener('input', (ev) => {
-    runEncryption();
+  document.getElementById(conf.ids.input).addEventListener('input', (ev) => {
+    runCryptFunction();
   });
 
-  document.getElementById(btnNewKey).addEventListener('click', () => {
+  document.getElementById(conf.ids.btnNewKey).addEventListener('click', () => {
     setNewKey();
-    runEncryption();
+    runCryptFunction();
   });
 
-  document.getElementById(btnEncrypt).addEventListener('click', () => {
-    runEncryption();
+  document.getElementById(conf.ids.btnEncrypt).addEventListener('click', () => {
+    runCryptFunction();
   });
 
-  document.getElementById(keyId).addEventListener('keydown', (ev) => {
-    if (allowedKeyKeys.includes(ev.key)) {
-      runEncryption();
+  document.getElementById(conf.ids.key).addEventListener('keydown', (ev) => {
+    if (allowedKeyInputKeys.includes(ev.key)) {
+      runCryptFunction();
     } else {
       ev.preventDefault();
       return false;
     }
   });
 
-  document.getElementById(btnReset).addEventListener('click', () => {
+  document.getElementById(conf.ids.btnReset).addEventListener('click', () => {
     setNewKey();
-    document.getElementById(inputId).value = '';
-    document.getElementById(outputId).value = '';
+    document.getElementById(conf.ids.input).value = '';
+    document.getElementById(conf.ids.output).value = '';
+  });
+
+  document.getElementById(conf.ids.isEncrypt).addEventListener('input', () => {
+    let isChecked = document.getElementById(conf.ids.isEncrypt).checked;
+    document.getElementById(conf.ids.titleName).innerHTML =
+      conf.checkboxOptions[isChecked].title;
+    document.getElementById(conf.ids.buttonName).innerHTML =
+      conf.checkboxOptions[isChecked].title;
+    runCryptFunction();
   });
 }
 
 window.onload = function () {
   setNewKey();
   createEventListeners();
+  document.getElementById(conf.ids.input).value = '';
+  document.getElementById(conf.ids.output).innerHTML = '';
+  document.getElementById(conf.ids.isEncrypt).checked = false;
 };
